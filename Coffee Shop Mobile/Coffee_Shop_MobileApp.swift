a69c3017a7d424cc2162a5276fcf833056774704
Thiago Santos <t.vaz.santos@gmail.com>
2025-12-06 20:45:54 -0300

    Initial Commit

---
+++ Coffee Shop Mobile/Coffee_Shop_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_Shop_MobileApp.swift
+//  Coffee Shop Mobile
+//
+//  Created by Thiago Santos on 06/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_Shop_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coffee Shop MobileUITests/Coffee_Shop_MobileUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Coffee_Shop_MobileUITestsLaunchTests.swift
+//  Coffee Shop MobileUITests
+//
+//  Created by Thiago Santos on 06/12/25.
+//
+
+import XCTest
+
+final class Coffee_Shop_MobileUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
